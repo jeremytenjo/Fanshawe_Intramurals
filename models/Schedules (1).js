4ee13d785874a8e99c8da03b7c55932a7fd710ce
Jeremy Tenjo <tenjojeremy@gmail.com>
2017-08-28 23:01:25 -0400

    Update

--- models/Schedules (1).js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var schedulesSchema = new Schema({
-    user: Schema.ObjectId,
-    times: Array
-});
-
-module.exports = mongoose.model('schedules', schedulesSchema);
